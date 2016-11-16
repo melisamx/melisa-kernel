efa130bf8f21ba493994c8b7525f2bf754702938
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-16 16:01:15 -0600

    add function info, debugLastQuery and make

--- Melisa/core/LogicBusiness.php
+++ Melisa/core/LogicBusiness.php
@@ -3,6 +3,8 @@
 trait LogicBusiness
 {
     
+    public $acts = [];
+    
     public function error($message, array $data = []) {
         
         return melisa('logger')->error($message, $data);
@@ -12,6 +14,33 @@ trait LogicBusiness
     public function debug($message, array $data = []) {
         
         return melisa('logger')->debug($message, $data);
+        
+    }
+    
+    public function info($message, array $data = []) {
+        
+        return melisa('logger')->info($message, $data);
+        
+    }
+    
+    public function debugLastQuery() {
+                
+        $queries = \DB::getQueryLog();
+        $lastQuery = end($queries);
+        
+        return melisa('logger')->debug($lastQuery['query']);
+        
+    }
+
+    public function make($key) {
+        
+        if( isset($this->acts[$key])) {
+            
+            return $this->acts[$key];
+            
+        }
+        
+        return $this->acts[$key] = app()->make($this->makes[$key]);
         
     }
     
