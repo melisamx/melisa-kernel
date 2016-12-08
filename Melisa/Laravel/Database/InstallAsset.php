090fafc4e7d3803651a9e5d6f2088c8cf8734f9b
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-12-08 10:34:42 -0600

    first conflict trait

---
+++ Melisa/Laravel/Database/InstallAsset.php
@@ -0,0 +1,40 @@
+<?php namespace Melisa\Laravel\Database;
+
+use Melisa\Laravel\Database\UpdateOrCreate;
+
+/**
+ * 
+ *
+ * @author Luis Josafat Heredia Contreras
+ */
+trait InstallAsset
+{
+    
+    public function installAssetCss($find, $values) {
+        
+        $values ['idAssetType']= 2;
+        
+        return $this->installAsset($find, $values);
+        
+    }    
+    
+    public function installAssetJs($find, $values) {
+        
+        $values ['idAssetType']= 1;
+        
+        return $this->installAsset($find, $values);
+                
+    }
+    
+    public function installAsset($find, $values) {
+        
+        return $this->UpdateOrCreate('App\Core\Models\Assets', [
+            'find'=>[
+                'id'=>$find
+            ],
+            'values'=>$values
+        ]);
+        
+    }
+    
+}
