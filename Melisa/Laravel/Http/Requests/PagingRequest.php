4fd955085bc6b848e8e88947c8ce05df2b72a111
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-01-10 09:28:38 -0600

    added paging request

---
+++ Melisa/Laravel/Http/Requests/PagingRequest.php
@@ -0,0 +1,19 @@
+<?php namespace Melisa\Laravel\Http\Requests;
+
+use Melisa\Laravel\Http\Requests\Generic;
+
+/**
+ * 
+ *
+ * @author Luis Josafat Heredia Contreras
+ */
+class PagingRequest extends Generic
+{
+    
+    protected $rules = [
+        'page'=>'required|numeric',
+        'start'=>'required|numeric',
+        'limit'=>'required|numeric',
+    ];
+    
+}
