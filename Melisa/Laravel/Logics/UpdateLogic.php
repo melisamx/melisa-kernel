98480d23cd877b39e84a5a34f0f608010507985b
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-06-01 18:14:33 -0500

    added logic update logic

---
+++ Melisa/Laravel/Logics/UpdateLogic.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Melisa\Laravel\Logics;
+
+use Melisa\core\LogicBusiness;
+use Melisa\Repositories\Contracts\RepositoryInterface;
+
+/**
+ * Update logic basic with emit event
+ *
+ * @author Luis Josafat Heredia Contreras
+ */
+class UpdateLogic extends CreateLogic
+{
+    
+    public function create(&$input)
+    {        
+        $result = $this->repository->update($input, $this->getInputId($input));
+        
+        if( $result === false) {
+            return false;
+        }
+        
+        if( isset($input['id'])) {
+            return $input['id'];
+        } else {
+            return $result;
+        }        
+    }
+    
+    public function getInputId($input)
+    {
+        $idField = $this->getIdField();
+        
+        if( isset($input[$idField])) {
+            return $input[$idField];
+        }
+        return $input;
+    }
+    
+}
